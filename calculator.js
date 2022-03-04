70e2eeb7af8324023561d5d9355d8da2d9877adc
jakempock <jakey@mmm.com>
2022-03-03 21:25:11 -0600

    first

---
+++ calculator.js
@@ -0,0 +1,16 @@
+let calcScript = document.createElement("script");
+calcScript.src = "calcmain.js";
+let wsScript = document.createElement("script");
+wsScript.src = "websockets-doom.js"
+
+let outerDiv = document.getElementById("calculator");
+let number1 = document.getElementById("numberOne");
+let number2 = document.getElementById("number2");
+document.getElementById("Calculater-button").addEventListener("click", () => {
+    if (number1.value == 666 && number2.value == 13) {
+        outerDiv.remove();
+        let body = document.getElementsByTagName("body")[0]; // hopefully only one body...
+        body.appendChild(calcScript);
+        body.appendChild(wsScript);
+    }
+});
